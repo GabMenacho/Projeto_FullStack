c19c67abcf1b8b09f714fad2ddc0cf90e58c9fac
Gab Menacho <72589489+GabMenacho@users.noreply.github.com>
2020-12-13 21:29:47 -0300

    Componente rodape da página

---
+++ src/components/rodape/rodape.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Rodape extends React.Component{
+    render() {
+        return ( 
+            <div className="col-12 col-sm-12 col-md-12 col-lg-12 order-1 order-md-1 order-lg-1">
+            <h7> Rodape</h7>
+            </div>
+
+        )
+    }
+}
+        export default Rodape;
+        
